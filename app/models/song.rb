12e57ebd8766fd488a4dd9a4975553bac3373991
Andrea Nasuto <andrea.nasuto@gmail.com>
2018-01-30 23:31:07 +0000

    automatically backed up by learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,2 +1,15 @@
 class Song < ActiveRecord::Base
+validates :title, presence: true, uniqueness: true
+ validates :released, inclusion: { in: [ true, false ] }
+ validates :released_year, presence: true, if: :has_been_released?
+ validate :check_release_year
+ validates :artist_name, presence: true
+
+ def has_been_released?
+   record.released == true
+ end
+
+ def check_release_year
+   self.released_year <= Time.new.year
+ end
 end
